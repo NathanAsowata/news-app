92f8dd6c096a6eeddde996832e7f8d05b2636f60
Nathan Asowata <nathanasowata@gmail.com>
2023-02-08 13:18:34 +0100

    chore: setup app skeleton

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Today's Headlines</title>
+        <link rel="shortcut icon" href="icon.png" type="image/x-icon" />
+      </Head>
+      <main>
+        <h1>The home page</h1>
+      </main>
+    </>
+  )
+}
+
+export default Home
